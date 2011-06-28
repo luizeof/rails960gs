02c1bab756d48c60b1833453221aa7a71ee04cd1
= <atendimento@orangeweb.com.br>
2011-06-28 20:03:55 -0300

    Initial commit to rails960gs.

---
+++ test/test_rails960gs.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestRails960gs < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
