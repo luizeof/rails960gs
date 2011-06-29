e3eab2b7f2e5a33d0dee70ea721a3bc2f077b593
= <atendimento@orangeweb.com.br>
2011-06-28 23:50:34 -0300

    some bug fixes .. releases v 0.2.1

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -1,10 +1,13 @@
-require 'rails960gs/view_helper'  
+require 'rails960gs/view_helper'        
+require 'rake'
+
 module Rails960gs
   
-  class Railtie < Rails::Railtie
-    initializer "rails960gs.view_helpers" do
-      ActionView::Base.send :include, ViewHelper
-    end  
+  class Railtie < Rails::Railtie  
     
+    initializer "rails960gs.view_helpers" do
+      ActionView::Base.send :include, ViewHelper           
+    end                                              
+
 end    
 end
